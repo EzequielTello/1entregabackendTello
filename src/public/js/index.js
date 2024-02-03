96a6a082f27ac2d9032b691f35a8730ae72e5226
Tu Nombre <ezetello23@gmail.com>
2024-02-02 21:42:55 -0300

    ultimos cambios

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -5,23 +5,17 @@ socket.on("updateProducts", (products) => {
   // products es la nueva lista de productos recibida desde el servidor
   console.log("Lista actualizada de productos:", products);
 
-  // Aquí puedes actualizar tu interfaz de usuario con la nueva lista de productos
-  // Por ejemplo, podrías renderizar una tabla HTML con los productos
   updateProductList(products);
 });
 
 // Función para actualizar la lista de productos en la interfaz de usuario
 function updateProductList(products) {
-  // Implementa la lógica para actualizar la interfaz aquí
-  // Por ejemplo, puedes usar DOM manipulation o cualquier framework que estés utilizando
-  // Ejemplo básico: actualiza la consola del navegador
   console.log(
     "Interfaz actualizada con la nueva lista de productos:",
     products
   );
 }
 
-// Resto de tu código existente...
 document
   .getElementById("productForm")
   .addEventListener("submit", function (event) {
